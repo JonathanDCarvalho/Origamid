77a81d0416e9881a0298c21d962c5db5c76d678f
JonathanDCarvalho <168686459+JonathanDCarvalho@users.noreply.github.com>
2025-03-27 14:42:04 -0300

    M11

---
+++ M11/script.js
@@ -0,0 +1,93 @@
+// const nav = document.querySelector("nav");
+// const produtos = document.querySelector(".produtos a");
+
+// console.log(nav);
+// console.log(produtos);
+
+// console.dir(nav);
+
+// nav.style.backgroundColor = "black";
+// nav.style.padding = "1 rem";
+
+// nav.classList.add("ativo");
+
+//
+
+// const nome = "O Senhor dos Anéis";
+// const ano = 1954;
+// const autor = "J. R. R. Tolkien";
+
+// const livro = {
+// 	nome: "O Senhor dos Anéis",
+// 	ano: 1954,
+// 	autor: "J. R. R. Tolkien",
+// };
+
+// const filme = {
+// 	nome: "O Senhor dos Anéis",
+// 	ano: 2001,
+// 	autor: "J. R. R. Tolkien",
+// 	diretor: "Peter Jackson",
+// };
+
+// console.log(livro.nome);
+
+// const texto = document.querySelector("p");
+// console.log(texto.clientHeight);
+
+// console.log(autor.toLowerCase());
+
+// const decimal = 122.99;
+
+// console.log(Number(decimal.toFixed()));
+// console.log(3);
+
+// const total = decimal.toFixed().length;
+
+// console.log(total);
+
+//
+
+// function livro(nome, ano, autor) {
+//   const nomeMaior = nome.toUpperCase();
+//   const totalAnos = 2050 - ano;
+//   const frase = nome + " por " + autor;
+
+//   const objeto = {
+//     nome: nomeMaior,
+//     totalAnos,
+//     frase,
+//   };
+//   return objeto;
+// }
+
+// const livroRetorno = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+// console.log(livroRetorno.frase);
+
+// const curso = {
+// 	nome: "JavaScript",
+// 	completar() {
+// 		console.log(this.nome);
+// 	},
+// };
+
+// curso.completar();
+
+// const ativar = document.querySelector(".ativar");
+
+// function handleClick(event) {
+//   console.log(event);
+// }
+
+// ativar.addEventListener('click', handleClick);
+
+// const botao = document.querySelector('.botao');
+
+// function mostrar() {
+//   const texto = document.querySelector('.texto');
+//   texto.classList.toggle('ativar');
+// }
+
+// botao.addEventListener('click', mostrar);
+
+console.log(window.innerWidth);
